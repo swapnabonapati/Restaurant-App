b850fba7ff9fc758757cf41b5b90bad0bcc32e15
swapna <swapnabgg97@gmail.com>
2024-06-18 05:19:28 +0000

    Restaurant App

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,9 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useContext} from 'react'
 
 import Header from '../Header'
 import DishItem from '../DishItem'
+
+import CartContext from '../../context/CartContext'
 
 import './index.css'
 
@@ -10,38 +12,7 @@ const Home = () => {
   const [response, setResponse] = useState([])
   const [activeCategoryId, setActiveCategoryId] = useState('')
 
-  const [cartItems, setCartItems] = useState([])
-
-  const addItemToCart = dish => {
-    const isAlreadyExists = cartItems.find(item => item.dishId === dish.dishId)
-    if (!isAlreadyExists) {
-      const newDish = {...dish, quantity: 1}
-      setCartItems(prev => [...prev, newDish])
-    } else {
-      setCartItems(prev =>
-        prev.map(item =>
-          item.dishId === dish.dishId
-            ? {...item, quantity: item.quantity + 1}
-            : item,
-        ),
-      )
-    }
-  }
-
-  const removeItemFromCart = dish => {
-    const isAlreadyExists = cartItems.find(item => item.dishId === dish.dishId)
-    if (isAlreadyExists) {
-      setCartItems(prev =>
-        prev
-          .map(item =>
-            item.dishId === dish.dishId
-              ? {...item, quantity: item.quantity - 1}
-              : item,
-          )
-          .filter(item => item.quantity > 0),
-      )
-    }
-  }
+  const {cartList, setRestaurantName} = useContext(CartContext)
 
   const getUpdatedData = tableMenuList =>
     tableMenuList.map(eachMenu => ({
@@ -63,11 +34,12 @@ const Home = () => {
     }))
 
   const fetchRestaurantApi = async () => {
-    const api = 'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
+    const api = 'https://run.mocky.io/v3/77a7e71b-804a-4fbd-822c-3e365d3482cc'
     const apiResponse = await fetch(api)
     const data = await apiResponse.json()
     const updatedData = getUpdatedData(data[0].table_menu_list)
     setResponse(updatedData)
+    setRestaurantName(data[0].restaurant_name)
     setActiveCategoryId(updatedData[0].menuCategoryId)
     setIsLoading(false)
   }
@@ -79,6 +51,10 @@ const Home = () => {
 
   const onUpdateActiveCategoryIdx = menuCategoryId =>
     setActiveCategoryId(menuCategoryId)
+
+  const addItemToCart = () => {}
+
+  const removeItemFromCart = () => {}
 
   const renderTabMenuList = () =>
     response.map(eachCategory => {
@@ -96,8 +72,8 @@ const Home = () => {
           onClick={onClickHandler}
         >
           <button
-            type='button'
-            className='mt-0 mb-0 ms-2 me-2 tab-category-button'
+            type="button"
+            className="mt-0 mb-0 ms-2 me-2 tab-category-button"
           >
             {eachCategory.menuCategory}
           </button>
@@ -111,12 +87,11 @@ const Home = () => {
     )
 
     return (
-      <ul className='m-0 d-flex flex-column dishes-list-container'>
+      <ul className="m-0 d-flex flex-column dishes-list-container">
         {categoryDishes.map(eachDish => (
           <DishItem
             key={eachDish.dishId}
             dishDetails={eachDish}
-            cartItems={cartItems}
             addItemToCart={addItemToCart}
             removeItemFromCart={removeItemFromCart}
           />
@@ -126,17 +101,17 @@ const Home = () => {
   }
 
   const renderSpinner = () => (
-    <div className='spinner-container'>
-      <div className='spinner-border' role='status' />
+    <div className="spinner-container">
+      <div className="spinner-border" role="status" />
     </div>
   )
 
   return isLoading ? (
     renderSpinner()
   ) : (
-    <div className='home-background'>
-      <Header cartItems={cartItems} />
-      <ul className='m-0 ps-0 d-flex tab-container'>{renderTabMenuList()}</ul>
+    <div className="home-background">
+      <Header cartItems={cartList} />
+      <ul className="m-0 ps-0 d-flex tab-container">{renderTabMenuList()}</ul>
       {renderDishes()}
     </div>
   )
